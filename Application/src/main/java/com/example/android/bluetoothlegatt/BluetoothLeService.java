414e2af1a0a8310ebadbc107521719baf1ccbed6
tomcat <tomcat.cheng@microlife.com.tw>
2016-04-07 10:34:17 +0800

    Modify service time out by variable.

--- Application/src/main/java/com/example/android/bluetoothlegatt/BluetoothLeService.java
+++ Application/src/main/java/com/example/android/bluetoothlegatt/BluetoothLeService.java
@@ -45,7 +45,6 @@ public class BluetoothLeService extends Service
     private final static String TAG = BluetoothLeService.class.getSimpleName();
     //public static final String COUNTDOWN_BR = "your_package_name.countdown_br";
 
-
     private BluetoothManager mBluetoothManager;
     private BluetoothAdapter mBluetoothAdapter;
     private String mBluetoothDeviceAddress;
@@ -58,6 +57,7 @@ public class BluetoothLeService extends Service
     private static final int STATE_DISCONNECTED = 0;
     private static final int STATE_CONNECTING = 1;
     private static final int STATE_CONNECTED = 2;
+    private static final int SERVICE_TIME_OUT = 30;     // every service connection auto time out.
 
     public final static String ACTION_GATT_CONNECTED = "com.example.bluetooth.le.ACTION_GATT_CONNECTED";
     public final static String ACTION_GATT_DISCONNECTED = "com.example.bluetooth.le.ACTION_GATT_DISCONNECTED";
@@ -216,7 +216,7 @@ public class BluetoothLeService extends Service
     {
         super.onCreate();
         Log.d(TAG, "Service countdown timer is setting.");
-        cdt = new CountDownTimer(30 * 1000, 1000)
+        cdt = new CountDownTimer(SERVICE_TIME_OUT * 1000, 1000)
         {
             @Override
             public void onTick(long millisUntilFinished)
