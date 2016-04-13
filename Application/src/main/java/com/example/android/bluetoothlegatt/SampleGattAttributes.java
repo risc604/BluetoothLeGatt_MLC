75c7b629a2142e5f34943e783bf95f5502dfbdf8
risc604 <risc604@ms40.url.com.tw>
2016-04-13 19:35:02 +0800

    modify Gatt Attributes.

--- Application/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
+++ Application/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
@@ -31,12 +31,12 @@ public class SampleGattAttributes {
 
     static {
         // Sample Services.
-        attributes.put("0000180d-0000-1000-8000-00805f9b34fb", "Heart Rate Service");
+        //attributes.put("0000180d-0000-1000-8000-00805f9b34fb", "Heart Rate Service");
         attributes.put("0000180a-0000-1000-8000-00805f9b34fb", "Device Information Service");
         attributes.put(MLC_BLE_SEVICE, "Microlife BLE device service");
         // Sample Characteristics.
-        attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
-        attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
+        //attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
+        //attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
         attributes.put(MLC_BLE_SEVICE_WRITE, "Microlife BLE device service write");
         attributes.put(MLC_BLE_SEVICE_READ, "Microlife BLE device service read");
     }
